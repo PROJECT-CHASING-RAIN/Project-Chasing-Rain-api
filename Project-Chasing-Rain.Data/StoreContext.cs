e1cefc9d91ee5816adc0aa9f4fcf855653351a14
Aidan Bush <aidanbush1@gmail.com>
2025-03-18 11:40:41 -0400

    Store context added

---
+++ Project-Chasing-Rain.Data/StoreContext.cs
@@ -0,0 +1,15 @@
+﻿using Project.Chasing.Rain.Domain.Catalog;
+using Microsoft.EntityFrameworkCore;
+
+namespace Jet.Piranha.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Item> Items { get; set; }
+    }
+}
